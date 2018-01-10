0fc9b2f1f15fa793deb014df6d69f948f3e3a9b3
cshepc <31074296+cshepc@users.noreply.github.com>
2018-01-10 09:08:50 -0800

    1/10/2018

--- Casey - guessgame.py
+++ Casey - guessgame.py
@@ -1,9 +1,12 @@
 import random
+
+# Initializing variables
 correct_guess = False
 rand_int = random.randint(0, 50)
 guesses = 5
 
-while correct_guess == False and guesses > 0:
+# Describes ONE turn (This is the game's Controller)
+while correct_guess is False and guesses > 0:
     player_guess = input("Guess a number between 0 and 50")
     player_guess = int(player_guess)
     if rand_int == player_guess:
--- Lucky7's.py
+++ Lucky7's.py
@@ -1,15 +1,20 @@
 import random
+
 balance = 15
+top_balance = 15
 number_of_turns = 0
+
 while balance > 0:
     number_of_turns += 1
-    #print("turn %s" % number_of_turns)
     die_1 = random.randint(1, 6)
     die_2 = random.randint(1, 6)
     if die_1 + die_2 == 7:
         balance += 5
+        if balance > top_balance:  # I added these two lines
+            top_balance = balance  # Because I was bored
     else:
         balance -= 1
 
 
-print("You started with 15 dollars. It took %s turns for you to run out of money" % number_of_turns)
+print('You started with 15 dollars. It took %s turns for you to run out of money' %  number_of_turns)
+print('Your top balance was %s dollars' % top_balance)
