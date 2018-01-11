db9999f07287697f7f1d31db515b3f3e56cb2ea9
cshepc <31074296+cshepc@users.noreply.github.com>
2018-01-11 09:45:21 -0800

    Started on blackjack
    1-11-2018

---
+++ Casey-Blackjack.py
@@ -0,0 +1,12 @@
+import random
+
+hearts = random.randint(1, 13)
+spades = random.randint(1, 13)
+diamonds = random.randint(1, 13)
+clubs = random.randint(1, 13)
+deck = []
+for deck in range(0,13):
+    
+
+
+
--- Casey-Lucky7's.py
+++ Casey-Lucky7's.py
@@ -1,15 +1,21 @@
 import random
+
 balance = 15
+top_balance = 15
 number_of_turns = 0
+top_turn = 0
 while balance > 0:
     number_of_turns += 1
-    #print("turn %s" % number_of_turns)
     die_1 = random.randint(1, 6)
     die_2 = random.randint(1, 6)
     if die_1 + die_2 == 7:
         balance += 5
+        if balance > top_balance:
+            top_balance = balance
+            top_turn = number_of_turns
     else:
         balance -= 1
 
 
-print("You started with 15 dollars. It took %s turns for you to run out of money" % number_of_turns)
+print('You started with 15 dollars. It took %s turns for you to run out of money' % number_of_turns)
+print('Your top balance was ' + str(top_balance) + ' dollars in round number ' +  str(top_turn))
