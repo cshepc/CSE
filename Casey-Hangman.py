12c20a0fdae2ba9981d5fbe00c7c2593b8918e83
cshepc <31074296+cshepc@users.noreply.github.com>
2018-01-22 09:47:04 -0800

    1.22.2018
    worked on hangman

--- Casey-Hangman.py
+++ Casey-Hangman.py
@@ -1,3 +1,5 @@
+import random
+import string
 '''
 This is a guide of how to make hangman
 
@@ -8,4 +10,24 @@ This is a guide of how to make hangman
 5. Create win and lose conditions
 '''
 
-word_bank = ["tortoise", "antidisestablishmentarianism", "hungry", "forgiveness", "inconsolable", "cataclysmic", "euphoria", "perjury", "melancholy", "photosynthesis"]
+word_bank = ["tortoise", "antidisestablishmentarianism", "hungry", "forgiveness", "inconsolable", "cataclysmic",
+             "euphoria", "perjury", "melancholy", "photosynthesis"]
+
+current_word = random.choice(word_bank)
+print(current_word)
+hidden_word = ""
+for word in range(len(current_word)):
+    hidden_word += "*"
+
+hidden_word = list(hidden_word)
+print(hidden_word)
+
+current_word = list(current_word)
+print(current_word)
+
+guess = input("Guess a letter")
+
+if guess in current_word:
+    print("h")
+else :
+    print("Nope! Guess again!")
