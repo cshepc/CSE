8a4938b793c35ebd8112afccd2603d648dc2d09e
cshepc <31074296+cshepc@users.noreply.github.com>
2018-01-23 09:47:08 -0800

    1.23.2018
    worked on hangman

--- Casey-Hangman.py
+++ Casey-Hangman.py
@@ -5,29 +5,31 @@ This is a guide of how to make hangman
 
 1. Make a word bank - 10 items
 2. Select a random item to guess
-3. Hide the word (use * or _)
-4. Reveal Letters based on input
-5. Create win and lose conditions
+3. Take in a letter and add it to a list of letters_guessed
+4. hide and reveal letters
+5. Create the win condition
 '''
 
-word_bank = ["tortoise", "antidisestablishmentarianism", "hungry", "forgiveness", "inconsolable", "cataclysmic",
-             "euphoria", "perjury", "melancholy", "photosynthesis"]
+word_bank = ["tortoise"]
+# , "antidisestablishmentarianism", "hungry", "forgiveness", "inconsolable", "cataclysmic",
+#          "euphoria", "perjury", "melancholy", "photosynthetic"]
+guesses_left = 10
 
 current_word = random.choice(word_bank)
 print(current_word)
-hidden_word = ""
-for word in range(len(current_word)):
-    hidden_word += "*"
-
-hidden_word = list(hidden_word)
-print(hidden_word)
-
 current_word = list(current_word)
 print(current_word)
 
-guess = input("Guess a letter")
 
-if guess in current_word:
-    print("h")
-else :
-    print("Nope! Guess again!")
+guesses = []
+output = []
+
+guesses.append(input("Guess a letter. >_"))
+for letter in current_word:
+    if letter in guesses:
+        output.append(guesses)
+    else:
+        output.append("*")
+
+    print(output)
+
