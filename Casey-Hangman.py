92b6f62ed1b9cc1e334bb8c8572a342bd5c7df6a
cshepc <31074296+cshepc@users.noreply.github.com>
2018-01-25 08:54:57 -0800

    1.24.18
    finished hangman

--- Casey-Hangman.py
+++ Casey-Hangman.py
@@ -10,26 +10,47 @@ This is a guide of how to make hangman
 5. Create the win condition
 '''
 
-word_bank = ["tortoise"]
-# , "antidisestablishmentarianism", "hungry", "forgiveness", "inconsolable", "cataclysmic",
-#          "euphoria", "perjury", "melancholy", "photosynthetic"]
-guesses_left = 10
+word_bank = ["tortoise", "antidisestablishmentarianism", "hungry", "forgiveness", "inconsolable", "cataclysmic",
+             "euphoria", "perjury", "melancholy", "photosynthetic"]
+def hangman():
+    play_again = True
+    user_choice = ''
+    while play_again is True:
+        guesses_left = 10
+        current_word = random.choice(word_bank)
+        # print(current_word)
+        print("*" * len(current_word))
+        list_word = list(current_word)
 
-current_word = random.choice(word_bank)
-print(current_word)
-current_word = list(current_word)
-print(current_word)
+
+        guesses = []
+        print("Your word has %i letters in it" % len(current_word))
+        while guesses_left > 0:
+            current_guess = input("Guess a letter. >_")
+            guesses.append(current_guess)
+
+            output = []
+            if current_guess not in list_word:
+                guesses_left -= 1
+            print("You have %s guesses left" % guesses_left)
+            for letter in list_word:
+                if current_guess == "stop":
+                    exit(0)
+                if letter in guesses:
+                    output.append(letter)
+                else:
+                    output.append("*")
+            output = ''.join(output)
+            if "*" not in output:
+                print("You win! the word you found was %s" % current_word)
+                user_choice = input("Play again? (y/n) >_")
+                if user_choice == "yes" or "y":
+                    print("\n\n\n")
+                    return
+                else:
+                    exit(0)
+        print("You lose! The word was %s" % current_word)
 
 
-guesses = []
-output = []
-
-guesses.append(input("Guess a letter. >_"))
-for letter in current_word:
-    if letter in guesses:
-        output.append(guesses)
-    else:
-        output.append("*")
-
-    print(output)
+hangman()
 
