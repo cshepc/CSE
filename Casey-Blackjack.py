894ded7d1e9cbd71be4a89113fa184fad9e3675a
cshepc <31074296+cshepc@users.noreply.github.com>
2018-01-30 09:46:12 -0800

    1.30.2018
    Worked on Blackjack

--- Casey-Blackjack.py
+++ Casey-Blackjack.py
@@ -1,54 +1,56 @@
 import random
 
-ace = 1
-jack = 10
-queen = 10
-king = 10
+deck = []
 
-deck = [ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king]
-suits = ["diamonds", "hearts", "spades", "clubs"]
-suits = suits * 13
-deck = deck * 4
-card1 = random.choice(deck)
+for card in range(0, 13):
+    card1 = card + 1
+    # print(card1)
+    suit1 = "Diamonds"
+    # print(suit1)
+    set1 = [card1, suit1]
+    deck.append(set1)
 
-suit1 = random.choice(suits)
+    card2 = card + 1
+    # print(card2)
+    suit2 = "Hearts"
+    # print(suit2)
+    set2 = [card2, suit2]
+    deck.append(set2)
 
-print(card1)
-print(suit1)
+    card3 = card + 1
+    # print(card3)
+    suit3 = "Spades"
+    # print(suit3)
+    set3 = [card3, suit3]
+    deck.append(set3)
 
-meme = []
+    card4 = card + 1
+    # print(card4)
+    suit4 = "Clubs"
+    # print(suit4)
+    set4 = [card4, suit4]
+    deck.append(set4)
 
-for card in range(0, 52):
+    # print(deck)
 
+callDeck = deck
+# print(callDeck)
+flat_deck = [item for sublist in callDeck for item in sublist]
+# print(flat_deck)
+flat_deck[0] = "Ace"
 
+for num in range(0, 26):
 
-if card1 == deck[ace]:
-    print("Ace of %s" % suit1)
-    suits.remove(suit1)
-    deck.remove(card1)
-    print(len(suits))
-    print(len(deck))
-elif card1 == deck[jack]:
-    print("Jack of %s" % suit1)
-    suits.remove(suit1)
-    deck.remove(card1)
-    print(len(suits))
-    print(len(deck))
-elif card1 == deck[queen]:
-    print("Queen of %s" % suit1)
-    suits.remove(suit1)
-    deck.remove(card1)
-    print(len(suits))
-    print(len(deck))
-elif card1 == deck[king]:
-    print("King of %s" % suit1)
-    suits.remove(suit1)
-    deck.remove(card1)
-    print(len(suits))
-    print(len(deck))
-else:
-    print("%i of %s" % (card1, suit1))
-    suits.remove(suit1)
-    deck.remove(card1)
-    print(len(suits))
-    print(len(deck))
+    if flat_deck[num + 1 * 2 - 1] == 1:
+        flat_deck[num + 1 * 2 - 1] = "Ace"
+
+    elif flat_deck[num + 1 * 2 - 1] == 11:
+        flat_deck[num + 1 * 2 - 1] = "Jack"
+
+    elif flat_deck[num + 1 * 2 - 1] == 12:
+        flat_deck[num + 1 * 2 - 1] = "Queen"
+
+    elif flat_deck[num + 1 * 2 - 1] == 13:
+        flat_deck[num + 1 * 2 - 1] = "King"
+
+print(flat_deck)
